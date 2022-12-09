a95dfc1934ef4681a939819ce4cb18da32fa3450
Victor <victorgson@outlook.com>
2022-12-09 14:20:37 +0100

    init

---
+++ team1-backend_csharp-quiz_api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace team1_backend_csharp_quiz_api;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
